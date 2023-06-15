add3c69d81bbf12312228d48812d3e1180894153
liveforone <yc4852@gmail.com>
2023-06-15 18:30:08 +0900

    Initial discovery-service.

---
+++ discovery-service/discovery-service/src/test/java/intelligent_store/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package intelligent_store.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
