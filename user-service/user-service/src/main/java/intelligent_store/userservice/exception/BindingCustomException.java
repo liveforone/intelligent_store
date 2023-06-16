9cf59b1aa4bba2aa5736b2cad14937659763a2a2
liveforone <yc4852@gmail.com>
2023-06-16 22:54:03 +0900

    Add binding and custom exception.

---
+++ user-service/user-service/src/main/java/intelligent_store/userservice/exception/BindingCustomException.java
@@ -0,0 +1,8 @@
+package intelligent_store.userservice.exception;
+
+public class BindingCustomException extends RuntimeException{
+
+    public BindingCustomException(String message) {
+        super(message);
+    }
+}
