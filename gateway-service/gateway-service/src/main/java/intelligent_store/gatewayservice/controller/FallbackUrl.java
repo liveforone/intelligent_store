3c3f5e2df38553f132e157dab114269fdb2e2117
liveforone <yc4852@gmail.com>
2023-06-16 20:18:31 +0900

    Initial gateway-service.

---
+++ gateway-service/gateway-service/src/main/java/intelligent_store/gatewayservice/controller/FallbackUrl.java
@@ -0,0 +1,7 @@
+package intelligent_store.gatewayservice.controller;
+
+public final class FallbackUrl {
+
+    public static final String BASE = "/fallback";
+    public static final String USER = "/user";
+}
