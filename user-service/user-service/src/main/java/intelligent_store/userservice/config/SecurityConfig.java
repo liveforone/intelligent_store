a86bbbd655f6436bce9d898cb3540da452cc19ae
liveforone <yc4852@gmail.com>
2023-06-16 22:52:39 +0900

    Add security config.

---
+++ user-service/user-service/src/main/java/intelligent_store/userservice/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package intelligent_store.userservice.config;
+
+import intelligent_store.userservice.controller.constant.MemberUrl;
+import intelligent_store.userservice.jwt.JwtAuthenticationFilter;
+import intelligent_store.userservice.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests((auth) -> auth
+                .requestMatchers(
+                        MemberUrl.SIGNUP_MEMBER,
+                        MemberUrl.SIGNUP_SELLER,
+                        MemberUrl.LOGIN,
+                        MemberUrl.MY_BANKBOOK_NUM,
+                        MemberUrl.MY_ADDRESS,
+                        MemberUrl.ACTUATOR
+                ).permitAll()
+                .anyRequest().authenticated()
+        );
+        http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.addFilterBefore(
+                new JwtAuthenticationFilter(jwtTokenProvider),
+                UsernamePasswordAuthenticationFilter.class
+        );
+        http.exceptionHandling((exception) -> exception.accessDeniedPage(MemberUrl.PROHIBITION));
+        return http.build();
+    }
+}
