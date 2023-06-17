bdb613751c0553f092ac9063a5cff50c984e77c7
liveforone <yc4852@gmail.com>
2023-06-17 11:51:41 +0900

    Add member command service.

---
+++ user-service/user-service/src/main/java/intelligent_store/userservice/command/MemberCommandService.java
@@ -0,0 +1,14 @@
+package intelligent_store.userservice.command;
+
+import intelligent_store.userservice.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class MemberCommandService {
+
+    private final MemberRepository memberRepository;
+}
