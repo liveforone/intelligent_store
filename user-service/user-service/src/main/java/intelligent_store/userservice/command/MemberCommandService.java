012e5d67683d1630896fdcbb6aea5b74ad1f2c47
liveforone <yc4852@gmail.com>
2023-06-17 11:57:44 +0900

    Add create member method.

--- user-service/user-service/src/main/java/intelligent_store/userservice/command/MemberCommandService.java
+++ user-service/user-service/src/main/java/intelligent_store/userservice/command/MemberCommandService.java
@@ -1,5 +1,8 @@
 package intelligent_store.userservice.command;
 
+import intelligent_store.userservice.domain.Member;
+import intelligent_store.userservice.domain.Role;
+import intelligent_store.userservice.dto.signupAndLogin.MemberSignupRequest;
 import intelligent_store.userservice.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,4 +14,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberCommandService {
 
     private final MemberRepository memberRepository;
+
+    public Long createMember(MemberSignupRequest requestDto) {
+        Member member = Member.create(
+                requestDto.getEmail(),
+                requestDto.getBankbookNum(),
+                requestDto.getPassword(),
+                requestDto.getRealName(),
+                Role.MEMBER,
+                requestDto.getCity(),
+                requestDto.getRoadNum(),
+                requestDto.getDetail()
+        );
+        return memberRepository.save(member).getId();
+    }
 }
