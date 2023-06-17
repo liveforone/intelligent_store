a7fc89a2a62303eb3ed8456fb8cae8897a0c8549
liveforone <yc4852@gmail.com>
2023-06-17 11:13:32 +0900

    Add find member query method.

--- user-service/user-service/src/main/java/intelligent_store/userservice/repository/MemberCustomRepository.java
+++ user-service/user-service/src/main/java/intelligent_store/userservice/repository/MemberCustomRepository.java
@@ -1,4 +1,12 @@
 package intelligent_store.userservice.repository;
 
+import intelligent_store.userservice.domain.Member;
+
+import java.util.Optional;
+
 public interface MemberCustomRepository {
+
+    Optional<Member> findOneById(Long id);
+    Optional<Member> findOneByEmail(String email);
+    Optional<Member> findOneByUsername(String username);
 }
--- user-service/user-service/src/main/java/intelligent_store/userservice/repository/MemberRepositoryImpl.java
+++ user-service/user-service/src/main/java/intelligent_store/userservice/repository/MemberRepositoryImpl.java
@@ -1,9 +1,12 @@
 package intelligent_store.userservice.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import intelligent_store.userservice.domain.Member;
 import intelligent_store.userservice.domain.QMember;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -11,4 +14,25 @@ public class MemberRepositoryImpl implements MemberCustomRepository {
 
     private final JPAQueryFactory queryFactory;
     QMember member = QMember.member;
+
+    public Optional<Member> findOneById(Long id) {
+        return Optional.ofNullable(queryFactory
+                .selectFrom(member)
+                .where(member.id.eq(id))
+                .fetchOne());
+    }
+
+    public Optional<Member> findOneByEmail(String email) {
+        return Optional.ofNullable(queryFactory
+                .selectFrom(member)
+                .where(member.email.eq(email))
+                .fetchOne());
+    }
+
+    public Optional<Member> findOneByUsername(String username) {
+        return Optional.ofNullable(queryFactory
+                .selectFrom(member)
+                .where(member.username.eq(username))
+                .fetchOne());
+    }
 }
