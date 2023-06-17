33985c04744360cd287ce65c7c2b89174cc8c124
liveforone <yc4852@gmail.com>
2023-06-17 11:04:36 +0900

    Add bankbook Response

---
+++ user-service/user-service/src/main/java/intelligent_store/userservice/dto/response/BankbookResponse.java
@@ -0,0 +1,14 @@
+package intelligent_store.userservice.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BankbookResponse {
+    private String bankbookNum;
+}
