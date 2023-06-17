496346cec2f2053b29eccd303691f3a25384f4a3
liveforone <yc4852@gmail.com>
2023-06-17 11:46:27 +0900

    Add get bankbook and address query service method.

--- user-service/user-service/src/main/java/intelligent_store/userservice/query/MemberQueryService.java
+++ user-service/user-service/src/main/java/intelligent_store/userservice/query/MemberQueryService.java
@@ -1,6 +1,8 @@
 package intelligent_store.userservice.query;
 
 import intelligent_store.userservice.controller.restResponse.ResponseMessage;
+import intelligent_store.userservice.dto.response.AddressResponse;
+import intelligent_store.userservice.dto.response.BankbookResponse;
 import intelligent_store.userservice.dto.response.MemberResponse;
 import intelligent_store.userservice.exception.MemberCustomException;
 import intelligent_store.userservice.query.util.MemberMapper;
@@ -28,5 +30,13 @@ public class MemberQueryService {
                 memberRepository.findOneByUsername(username)
                         .orElseThrow(() -> new MemberCustomException(ResponseMessage.MEMBER_IS_NULL))
         );
+    }
+
+    public BankbookResponse getBankbookByUsername(String username) {
+        return memberRepository.findBankbookNumByUsername(username);
+    }
+
+    public AddressResponse getAddressByUsername(String username) {
+        return memberRepository.findAddressByUsername(username);
     }
 }
