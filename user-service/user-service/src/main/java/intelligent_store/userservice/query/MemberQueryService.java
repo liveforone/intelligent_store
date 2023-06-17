ff953003f7b24bd54e08a9c50d589080876f53b2
liveforone <yc4852@gmail.com>
2023-06-17 11:44:03 +0900

    Add get member query service method.

--- user-service/user-service/src/main/java/intelligent_store/userservice/query/MemberQueryService.java
+++ user-service/user-service/src/main/java/intelligent_store/userservice/query/MemberQueryService.java
@@ -1,5 +1,9 @@
 package intelligent_store.userservice.query;
 
+import intelligent_store.userservice.controller.restResponse.ResponseMessage;
+import intelligent_store.userservice.dto.response.MemberResponse;
+import intelligent_store.userservice.exception.MemberCustomException;
+import intelligent_store.userservice.query.util.MemberMapper;
 import intelligent_store.userservice.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,5 +16,17 @@ public class MemberQueryService {
 
     private final MemberRepository memberRepository;
 
+    public MemberResponse getMemberByEmail(String email) {
+        return MemberMapper.entityToDto(
+                memberRepository.findOneByEmail(email)
+                        .orElseThrow(() -> new MemberCustomException(ResponseMessage.MEMBER_IS_NULL))
+        );
+    }
 
+    public MemberResponse getMemberByUsername(String username) {
+        return MemberMapper.entityToDto(
+                memberRepository.findOneByUsername(username)
+                        .orElseThrow(() -> new MemberCustomException(ResponseMessage.MEMBER_IS_NULL))
+        );
+    }
 }
