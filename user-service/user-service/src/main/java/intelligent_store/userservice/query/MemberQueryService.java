51196d17793f46c4a14ffe56ce1e575a7c484411
liveforone <yc4852@gmail.com>
2023-06-17 11:22:19 +0900

    Add member query service.

---
+++ user-service/user-service/src/main/java/intelligent_store/userservice/query/MemberQueryService.java
@@ -0,0 +1,16 @@
+package intelligent_store.userservice.query;
+
+import intelligent_store.userservice.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberQueryService {
+
+    private final MemberRepository memberRepository;
+
+
+}
