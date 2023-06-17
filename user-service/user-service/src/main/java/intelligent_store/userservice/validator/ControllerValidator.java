2339d3f619c10e2cdbc924c6984dcf3349ddf38e
liveforone <yc4852@gmail.com>
2023-06-17 12:16:26 +0900

    Add controller validator.

---
+++ user-service/user-service/src/main/java/intelligent_store/userservice/validator/ControllerValidator.java
@@ -0,0 +1,20 @@
+package intelligent_store.userservice.validator;
+
+import intelligent_store.userservice.exception.BindingCustomException;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+
+import java.util.Objects;
+
+@Component
+public class ControllerValidator {
+
+    public void validateBinding(BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            String errorMessage = Objects
+                    .requireNonNull(bindingResult.getFieldError())
+                    .getDefaultMessage();
+            throw new BindingCustomException(errorMessage);
+        }
+    }
+}
