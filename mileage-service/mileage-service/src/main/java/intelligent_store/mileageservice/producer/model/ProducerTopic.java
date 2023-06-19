c5d71503d29e4e692c7d4aa75ad7dda35c98c57e
liveforone <yc4852@gmail.com>
2023-06-19 11:30:17 +0900

    Add producer topic.

---
+++ mileage-service/mileage-service/src/main/java/intelligent_store/mileageservice/producer/model/ProducerTopic.java
@@ -0,0 +1,8 @@
+package intelligent_store.mileageservice.producer.model;
+
+public final class ProducerTopic {
+
+    private ProducerTopic() {}
+
+    public static final String USE_MILEAGE_FAIL_ROLLBACK_ORDER = "use-mileage-fail-rollback-order";
+}
