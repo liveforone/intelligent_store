9f03e3146f912be6f621a073acec3197dcde40b0
liveforone <yc4852@gmail.com>
2023-06-19 17:32:01 +0900

    Add get mileage my username method.

--- mileage-service/mileage-service/src/main/java/intelligent_store/mileageservice/service/query/MileageQueryService.java
+++ mileage-service/mileage-service/src/main/java/intelligent_store/mileageservice/service/query/MileageQueryService.java
@@ -1,5 +1,7 @@
 package intelligent_store.mileageservice.service.query;
 
+import intelligent_store.mileageservice.dto.response.MileageResponse;
+import intelligent_store.mileageservice.dto.util.MileageMapper;
 import intelligent_store.mileageservice.repository.MileageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,5 +14,7 @@ public class MileageQueryService {
 
     private final MileageRepository mileageRepository;
 
-
+    public MileageResponse getMileageByUsername(String username) {
+        return MileageMapper.entityToDto(mileageRepository.findOneByUsername(username));
+    }
 }
