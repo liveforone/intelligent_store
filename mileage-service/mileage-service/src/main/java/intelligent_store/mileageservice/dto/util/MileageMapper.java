12f5ab0db74982465d9d09c0a8b83b0f6ded8665
liveforone <yc4852@gmail.com>
2023-06-19 17:26:13 +0900

    Add entity to dto mapper.

--- mileage-service/mileage-service/src/main/java/intelligent_store/mileageservice/dto/util/MileageMapper.java
+++ mileage-service/mileage-service/src/main/java/intelligent_store/mileageservice/dto/util/MileageMapper.java
@@ -1,6 +1,15 @@
 package intelligent_store.mileageservice.dto.util;
 
+import intelligent_store.mileageservice.domain.Mileage;
+import intelligent_store.mileageservice.dto.response.MileageResponse;
+
 public class MileageMapper {
 
-
+    public static MileageResponse entityToDto(Mileage mileage) {
+        return MileageResponse
+                .builder()
+                .id(mileage.getId())
+                .mileagePoint(mileage.getMileagePoint())
+                .build();
+    }
 }
