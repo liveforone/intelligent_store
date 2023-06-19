d9226ef77d5ae40e1f528eae799ae65452e1c25e
liveforone <yc4852@gmail.com>
2023-06-19 17:28:05 +0900

    Remove type change method.

--- mileage-service/mileage-service/src/main/java/intelligent_store/mileageservice/dto/request/MileageRequestWhenOrder.java
+++ mileage-service/mileage-service/src/main/java/intelligent_store/mileageservice/dto/request/MileageRequestWhenOrder.java
@@ -1,6 +1,5 @@
 package intelligent_store.mileageservice.dto.request;
 
-import intelligent_store.mileageservice.dto.request.MileageFailRollbackOrderRequest;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,12 +9,4 @@ public class MileageRequestWhenOrder {
     private long itemPrice;
     private long spentMileage;
     private String username;
-
-    public MileageFailRollbackOrderRequest toMileageFailRollbackOrderDto() {
-        return MileageFailRollbackOrderRequest.builder()
-                .itemPrice(itemPrice)
-                .spentMileage(spentMileage)
-                .username(username)
-                .build();
-    }
 }
