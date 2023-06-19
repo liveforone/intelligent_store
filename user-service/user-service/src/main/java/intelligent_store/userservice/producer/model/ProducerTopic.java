3e0ac0aeb17edb4967b63570cf1ac9ff96c92c97
liveforone <yc4852@gmail.com>
2023-06-19 11:17:57 +0900

    Add remove mileage topic.

--- user-service/user-service/src/main/java/intelligent_store/userservice/producer/model/ProducerTopic.java
+++ user-service/user-service/src/main/java/intelligent_store/userservice/producer/model/ProducerTopic.java
@@ -4,6 +4,7 @@ public final class ProducerTopic {
 
     private ProducerTopic() {}
 
-    public static final String REMOVE_SHOP_BELONG_MEMBER = "remove-shop-belong-member";
     public static final String CREATE_MILEAGE = "create-mileage";
+    public static final String REMOVE_MILEAGE_BELONG_MEMBER = "remove-mileage-belong-member";
+    public static final String REMOVE_SHOP_BELONG_MEMBER = "remove-shop-belong-member";
 }
