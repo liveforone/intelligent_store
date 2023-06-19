fa9122f911f9210bbde26644fec9cd2ba961ca1c
liveforone <yc4852@gmail.com>
2023-06-19 17:31:39 +0900

    Add find one by username query method.

--- mileage-service/mileage-service/src/main/java/intelligent_store/mileageservice/repository/MileageCustomRepository.java
+++ mileage-service/mileage-service/src/main/java/intelligent_store/mileageservice/repository/MileageCustomRepository.java
@@ -1,4 +1,7 @@
 package intelligent_store.mileageservice.repository;
 
+import intelligent_store.mileageservice.domain.Mileage;
+
 public interface MileageCustomRepository {
+    Mileage findOneByUsername(String username);
 }
--- mileage-service/mileage-service/src/main/java/intelligent_store/mileageservice/repository/MileageRepositoryImpl.java
+++ mileage-service/mileage-service/src/main/java/intelligent_store/mileageservice/repository/MileageRepositoryImpl.java
@@ -1,14 +1,22 @@
 package intelligent_store.mileageservice.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import intelligent_store.mileageservice.domain.Mileage;
 import intelligent_store.mileageservice.domain.QMileage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
-public class MileageRepositoryImpl {
+public class MileageRepositoryImpl implements MileageCustomRepository {
 
     private final JPAQueryFactory queryFactory;
     QMileage mileage = QMileage.mileage;
+
+    public Mileage findOneByUsername(String username) {
+        return queryFactory
+                .selectFrom(mileage)
+                .where(mileage.username.eq(username))
+                .fetchOne();
+    }
 }
