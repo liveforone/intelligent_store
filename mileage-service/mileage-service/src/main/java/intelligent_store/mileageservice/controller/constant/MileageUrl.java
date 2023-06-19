aa9e4486d48e4b717b3e6382d4bd24ef1e942fed
liveforone <yc4852@gmail.com>
2023-06-19 18:21:42 +0900

    Add my mileage get and post url.

---
+++ mileage-service/mileage-service/src/main/java/intelligent_store/mileageservice/controller/constant/MileageUrl.java
@@ -0,0 +1,7 @@
+package intelligent_store.mileageservice.controller.constant;
+
+public final class MileageUrl {
+
+    public static final String MY_MILEAGE_GET = "/provide/my-mileage/{username}";
+    public static final String MY_MILEAGE_POST = "/provide/my-mileage";
+}
