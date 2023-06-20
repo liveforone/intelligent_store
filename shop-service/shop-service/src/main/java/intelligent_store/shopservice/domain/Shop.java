72455a8ebc0776a5222623c50958ba2cf2ca4a25
liveforone <yc4852@gmail.com>
2023-06-20 18:04:57 +0900

    Add shop entity.

---
+++ shop-service/shop-service/src/main/java/intelligent_store/shopservice/domain/Shop.java
@@ -0,0 +1,31 @@
+package intelligent_store.shopservice.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Shop {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String shopName;
+
+    @Column(nullable = false)
+    private String tel;
+
+    private Shop(String username, String shopName, String tel) {
+        this.username = username;
+        this.shopName = shopName;
+        this.tel = tel;
+    }
+}
