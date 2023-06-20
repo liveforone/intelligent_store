c3b05af1a05bf376d059223d8ff3dccc4f558749
liveforone <yc4852@gmail.com>
2023-06-20 16:54:10 +0900

    Add remove recommend topic.

--- shop-service/shop-service/src/main/java/intelligent_store/shopservice/producer/model/ProducerTopic.java
+++ shop-service/shop-service/src/main/java/intelligent_store/shopservice/producer/model/ProducerTopic.java
@@ -4,5 +4,6 @@ public final class ProducerTopic {
 
     private ProducerTopic() {}
 
+    public static final String REMOVE_RECOMMEND_BELONG_SHOP = "remove-recommend-belong-shop";
     public static final String REMOVE_ITEM_BELONG_SHOP = "remove-item-belong-shop";
 }
