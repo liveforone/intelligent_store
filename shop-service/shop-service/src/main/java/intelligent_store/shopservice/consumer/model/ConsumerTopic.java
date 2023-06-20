6fc7053ff41af680cac64841aebf77dfe4e962cb
liveforone <yc4852@gmail.com>
2023-06-20 17:02:40 +0900

    Add consumer topic.

---
+++ shop-service/shop-service/src/main/java/intelligent_store/shopservice/consumer/model/ConsumerTopic.java
@@ -0,0 +1,8 @@
+package intelligent_store.shopservice.consumer.model;
+
+public final class ConsumerTopic {
+    private ConsumerTopic() {}
+
+    public static final String CREATE_SHOP = "create-shop";
+    public static final String REMOVE_SHOP_BELONG_MEMBER = "remove-shop-belong-member";
+}
