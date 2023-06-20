f48168dedfca4477f1f803c231214095a2848a25
liveforone <yc4852@gmail.com>
2023-06-20 18:01:59 +0900

    Add recommend url.

---
+++ shop-service/shop-service/src/main/java/intelligent_store/shopservice/feignClient/constant/RecommendUrl.java
@@ -0,0 +1,8 @@
+package intelligent_store.shopservice.feignClient.constant;
+
+public final class RecommendUrl {
+    private RecommendUrl() {}
+
+    public static final String BASE = "recommend-service";
+    public static final String GET_RECOMMEND_SHOP_ID = "/provide/recommend-shop";
+}
