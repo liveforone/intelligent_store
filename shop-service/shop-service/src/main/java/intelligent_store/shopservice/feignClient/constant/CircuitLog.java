f788a73196be17f9e28240799b45cafff586beb3
liveforone <yc4852@gmail.com>
2023-06-20 18:01:46 +0900

    Add circuit log.

---
+++ shop-service/shop-service/src/main/java/intelligent_store/shopservice/feignClient/constant/CircuitLog.java
@@ -0,0 +1,13 @@
+package intelligent_store.shopservice.feignClient.constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum CircuitLog {
+
+    SHOP_CIRCUIT_LOG("shop-circuit");
+
+    private final String value;
+}
